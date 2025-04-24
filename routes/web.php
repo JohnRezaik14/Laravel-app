31a657918963ab43a87aa655035ca6413337e56b
John Rezaik <johnrezaik@gmail.com>
2025-04-24 15:55:41 +0200

    handle routing

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('');
 
 // Route::resource for PostsController
-Route::resource('posts', PostsController::class);
 
 Route::middleware('auth')->group(function () {
+    Route::resource('posts', PostsController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
 //! error 404 fallback route
 Route::fallback(function () {
     return view('NotFound');
